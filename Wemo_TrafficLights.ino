5d36a3a7b9d085a6b9586b0c8a303287933550f6
Sh0rtyTheCircuit <arian.cornel@gmail.com>
2018-10-02 14:28:40 -0600

    Pins Added
    Code does not work

--- Wemo_TrafficLights.ino
+++ Wemo_TrafficLights.ino
@@ -1,46 +1,74 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+
+#include <WiFiClient.h>
+
+
+int GREEN = D1; //DGREEN
+int YELLOW = D2;
+int RED = D4;
 
 void setup() {
   // put your setup code here, to run once:
-int GREEN = 8; //D8
-int YELLOW = 6;
-int RED = 0;
+  pinMode(GREEN,OUTPUT);
+  pinMode(YELLOW,OUTPUT);
+  pinMode(RED,OUTPUT);
+  Serial.begin(115200);
 }
 
-TurnGREEN(){
-  digitalWrite(8,HIGH);
-  digitalWrite(6,LOW);
-  digitalWrite(0,LOW);
+void TurnGREEN(){
+  TurnOFF();
+  digitalWrite(GREEN,HIGH);
+  digitalWrite(YELLOW,LOW);
+  digitalWrite(RED,LOW);
+  Serial.print("GREEN");
 }
 
-TurnYELLOW(){
-  digitalWrite(6,HIGH);
-  digitalWrite(8,LOW);
-  digitalWrite(0,LOW);
+void TurnYELLOW(){
+  TurnOFF();
+  digitalWrite(YELLOW,HIGH);
+  digitalWrite(GREEN,LOW);
+  digitalWrite(RED,LOW);
+  Serial.print("YELLOW");
 }
 
-TurnRED(){
-  digitalWrite(0,HIGH);
-  digitalWrite(8,LOW);
-  digitalWrite(6,LOW);
+void TurnRED(){
+  TurnOFF();
+  digitalWrite(RED,HIGH);
+  digitalWrite(GREEN,LOW);
+  digitalWrite(YELLOW,LOW);
+  Serial.print("RED");
 }
 
-TurnOFF(){
-  digitalWrite(8,LOW);
-  digitalWrite(6,LOW);
-  digitalWrite(0,LOW);
+void TurnOFF(){
+  TurnOFF();
+  digitalWrite(GREEN,LOW);
+  digitalWrite(YELLOW,LOW);
+  digitalWrite(RED,LOW);
+  Serial.print("TurnedOFF");
 }
 
-CYCLE(){
-  digitalWrite(8,HIGH);
+/*void CYCLE(){
+//  digitalWrite(GREEN,HIGH);
   delay(2000); //Wait 2 seconds
-  digitalWrite(8,LOW);
-  digitalWrite(6,HIGH);
+  digitalWrite(GREEN,LOW);
+  digitalWrite(YELLOW,HIGH);
   delay(2000);
-  digitalWrite(6,LOW);
-  digitalWrite(0,HIGH);
-}
+  digitalWrite(YELLOW,LOW);
+  digitalWrite(RED,HIGH);
+  delay(2000);
+}*/
 
 void loop() {
   // put your main code here, to run repeatedly:
+  TurnOFF();
   TurnGREEN();
+  delay(1000);
+  TurnRED();
+  delay(1000);
+  TurnYELLOW();
+  delay(1000);
+  Serial.print("Looped");
 }
