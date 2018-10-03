7f56a36c630123f7488f7f452c1112eeb8f600a6
Sh0rtyTheCircuit <arian.cornel@gmail.com>
2018-10-03 17:17:24 -0600

    It's Alive!
    Code works. Seperate functions made for turning off all the lights/clearing them. One will send the server info about the page, the other actually turns off all the leds.

--- Wemo_TrafficLights.ino
+++ Wemo_TrafficLights.ino
@@ -2,20 +2,87 @@
 #include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>
 #include <WiFiClient.h>
-#include <SPI.h>
 //#include <avr/wdt.h> Watchdog timer library
 
 // #### LED Pin Setup #### //
-int GREEN = D1; 
-int YELLOW = D2;
-int RED = D4;
+int GREEN = D5; 
+int YELLOW = D7;
+int RED = D6;
 
 // ##### Wifi Connection Setup #### //
-char WifiName[] = 
-"Verizon-SM-G935V";            //SSID
+char WifiName[] = "Verizon-SM-G935V";            //SSID
 char Password[] = "password";
 ESP8266WebServer server(80);                     //Server is on Port 80
 
+
+// #### Web Page Setup #### //
+char WebPage[] = "<html><title><Choose Wisely></title><body><form action=\"/green\"><button>Green</button></form><br><form action=\"/yellow\"><button>Yellow</button></form><br><form action=\"/red\"><button>Red</button></form><br><form action=\"/turnoff\"><button>Clear</button></form><br><form action=\"/cycle\"><button>Cycle</button></form><br></body></html>";
+char AutoRespond[] = "text/html\nRefresh: 1";   //header: content type/conent type\ how often refresh
+
+void NoClient(){
+  server.send(404);
+  Serial.println("Reconnecting to server");
+}
+
+void HOME(){                //HomePage
+  server.send(302, AutoRespond, WebPage);
+  Serial.println("Home Page Opened");
+}
+
+// #### LED Functions Setup #### //
+void TurnGREEN(){
+  //TurnOFF();
+  AllClear();
+  digitalWrite(GREEN,HIGH);
+  //digitalWrite(YELLOW,LOW);
+  //digitalWrite(RED,LOW);
+  Serial.println("GREEN");
+  server.send(302, AutoRespond, WebPage);
+}
+
+void TurnYELLOW(){
+  //TurnOFF();
+  AllClear();
+  digitalWrite(YELLOW,HIGH);
+  //digitalWrite(GREEN,LOW);
+  //digitalWrite(RED,LOW);
+  Serial.println("YELLOW");
+  server.send(302, AutoRespond, WebPage);
+}
+
+void TurnRED(){
+  AllClear();
+  digitalWrite(RED,HIGH);
+  Serial.println("RED");
+  server.send(302, AutoRespond, WebPage);
+}
+
+void TurnOFF(){
+  AllClear();
+  Serial.println("All Clear");
+  server.send(302, AutoRespond, WebPage);
+}
+
+void AllClear(){
+  digitalWrite(GREEN,LOW);
+  digitalWrite(YELLOW,LOW);
+  digitalWrite(RED,LOW);
+}
+
+void CYCLE(){
+  //TurnOFF();
+  AllClear();
+  Serial.println("Cycling");
+  digitalWrite(GREEN,HIGH);
+  delay(2000); //Wait 2 seconds
+  digitalWrite(GREEN,LOW);
+  digitalWrite(YELLOW,HIGH);
+  delay(2000);
+  digitalWrite(YELLOW,LOW);
+  digitalWrite(RED,HIGH);
+  delay(2000);
+  server.send(302, AutoRespond, WebPage);
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -36,76 +103,17 @@ void setup() {
 
 // #### Activate Functions #### //
   //If "/ " is seen in the URL, do this function
-  server.on("/GREEN", TurnGREEN);
-  server.on("/YELLOW", TurnYELLOW);
-  server.on("/RED", TurnRED);
-  server.on("/TurnOFF", TurnOFF);
-  server.on ("/Cycle", CYCLE);
+  server.on("/green", TurnGREEN);
+  server.on("/yellow", TurnYELLOW);
+  server.on("/red", TurnRED);
+  server.on("/turnoff", TurnOFF);
+  server.on ("/cycle", CYCLE);
+  server.on("/", HOME);
 
   server.onNotFound(NoClient);            //When client not found
 
   server.begin();
   Serial.println("Server Ready");
-}
-
-
-// #### Web Page Setup #### //
-char WebPage[] = "<html><title><Choose Wisely></title><body><form action=\"/GREEN\"><button>Green</button></form><br><form action=\"/YELLOW\"><button>Yellow</button></form><br><form action=\"/RED\"><button>Red</button></form><br><form action=\"/TurnOFF\"><button>Clear</button></form><br><form action=\"/CYCLE\"><button>Cycle</button></form><br></body></html>";
-char AutoRespond[] = "text/html\nRefresh: 1";   //header: content type/conent type\ how often refresh
-
-void NoClient(){
-  server.send(302, AutoRespond, WebPage);
-  Serial.println("Reconnecting to server");
-}
-
-// #### LED Functions Setup #### //
-void TurnGREEN(){
-  TurnOFF();
-  digitalWrite(GREEN,HIGH);
-  digitalWrite(YELLOW,LOW);
-  digitalWrite(RED,LOW);
-  Serial.print("GREEN");
-  server.send(302, AutoRespond, WebPage);
-}
-
-void TurnYELLOW(){
-  TurnOFF();
-  digitalWrite(YELLOW,HIGH);
-  digitalWrite(GREEN,LOW);
-  digitalWrite(RED,LOW);
-  Serial.print("YELLOW");
-  server.send(302, AutoRespond, WebPage);
-}
-
-void TurnRED(){
-  TurnOFF();
-  digitalWrite(RED,HIGH);
-  digitalWrite(GREEN,LOW);
-  digitalWrite(YELLOW,LOW);
-  Serial.print("RED");
-  server.send(302, AutoRespond, WebPage);
-}
-
-void TurnOFF(){
-  TurnOFF();
-  digitalWrite(GREEN,LOW);
-  digitalWrite(YELLOW,LOW);
-  digitalWrite(RED,LOW);
-  Serial.print("TurnedOFF");
-  server.send(302, AutoRespond, WebPage);
-}
-
-void CYCLE(){
-  TurnOFF();
-  digitalWrite(GREEN,HIGH);
-  delay(2000); //Wait 2 seconds
-  digitalWrite(GREEN,LOW);
-  digitalWrite(YELLOW,HIGH);
-  delay(2000);
-  digitalWrite(YELLOW,LOW);
-  digitalWrite(RED,HIGH);
-  delay(2000);
-  server.send(302, AutoRespond, WebPage);
 }
 
 void loop() {                                // put your main code here, to run repeatedly:
